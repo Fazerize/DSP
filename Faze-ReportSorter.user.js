8a8fc26a97d2a6b93519876c6b274dee2e8f0c57
fazerize <166784865+fazerize@users.noreply.github.com>
2024-05-23 09:56:38 +0200

    Added All and WG accept button, made functions for selection to make it easier for the all button

--- Faze-ReportSorter.user.js
+++ Faze-ReportSorter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Faze - Report Sorter
-// @version      0.5
+// @version      0.6
 // @author       Faze
 // @description  Sortiert Berichte
 // @run-at       document-idle
@@ -20,84 +20,140 @@ let cell = $(
   "<td style='vertical-align: center; text-align:center;' colspan='4'></td>"
 );
 
+function selectRZ() {
+  $("img[src*='report_scavenging']")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+function selectMP() {
+  $("img[src*='report_trade']")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+function selectUT() {
+  $('.quickedit-label:contains("Deine Unterstützung")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains("schickt die Unterstützung")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains("zieht seine Unterstützung")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains("Stammesunterstützung")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains(" unterstützt ")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+function selectAM() {
+  $('.quickedit-label:contains("Warteschlange abgeschlossen")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains("Warteschlange unterbrochen")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+function selectSonst() {
+  $('.quickedit-label:contains("Adelsreservierung")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains("Reserviertes Dorf")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+  $('.quickedit-label:contains("Erfolg erhalten")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+function selectWG() {
+  $('.quickedit-label:contains("Weitergeleitete Berichte von")')
+    .closest(".quickedit.report-title")
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+function selectWGArch() {
+  $("img[src*='forwarded']")
+    .parent()
+    .parent()
+    .parent()
+    .parent()
+    .prev()
+    .find("input")
+    .prop("checked", true);
+}
+
 function sortReports(name) {
-  if (name == "Raubzug löschen") {
-    $("img[src*='report_scavenging']")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+  if (name == "Alle löschen") {
+    selectRZ();
+    selectMP();
+    selectUT();
+    selectAM();
+    selectSonst();
     $(".btn-cancel").click();
   }
-  if (name == "Handel löschen") {
-    $("img[src*='report_trade']")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+  if (name == "RZ löschen") {
+    selectRZ();
     $(".btn-cancel").click();
   }
-  if (name == "Unterstützung löschen") {
-    $('.quickedit-label:contains("Deine Unterstützung")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
-      $('.quickedit-label:contains("schickt die Unterstützung")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+  if (name == "MP löschen") {
+    selectMP();
     $(".btn-cancel").click();
-    $('.quickedit-label:contains("Stammesunterstützung")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+  }
+  if (name == "UT löschen") {
+    selectUT();
     $(".btn-cancel").click();
   }
   if (name == "AM löschen") {
-    $('.quickedit-label:contains("Warteschlange abgeschlossen")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
-    $('.quickedit-label:contains("Warteschlange unterbrochen")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+    selectAM();
     $(".btn-cancel").click();
   }
-  if (name == "Sonstiges löschen") {
-    $('.quickedit-label:contains("Adelsreservierung")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
-    $('.quickedit-label:contains("Reserviertes Dorf")')
-      .closest(".quickedit.report-title")
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+  if (name == "Sonst. löschen") {
+    selectSonst();
     $(".btn-cancel").click();
   }
-  if (name == "Weitergeleitet archivieren") {
-    $("img[src*='forwarded']")
-      .parent()
-      .parent()
-      .parent()
-      .parent()
-      .prev()
-      .find("input")
-      .prop("checked", true);
+  if (name == "WG annehmen") {
+    selectWG();
+    $(".btn[name='accept_forwarded']").click();
+  }
+  if (name == "WG archivieren") {
+    selectWGArch();
     $('select[name*="group_id"] option:contains("Archiv")').prop(
       "selected",
       true
@@ -108,26 +164,42 @@ function sortReports(name) {
 
 if (location.href.includes("view=") === false) {
   //Buttonleiste in Berichte im Kopf verschieben
-  const reportTable = document.getElementById('report_list');
+  const reportTable = document.getElementById("report_list");
 
-  if(location.href.includes('group_id=')) {
-      var reportActionForm = document.getElementsByTagName('form')[7];
-  } else if(location.href.includes('mode=all') || location.href.includes('mode=attack') || location.href.includes('mode=defense')) {
-      var reportActionForm = document.getElementsByTagName('form')[7];
-  } else if(location.href.includes('mode=trade') || location.href.includes('mode=support')) {
-      var reportActionForm = document.getElementsByTagName('form')[4];
-  } else if(location.href.includes('mode=') || location.href.includes('mode=forwarded')) {
-      var reportActionForm = document.getElementsByTagName('form')[2];
+  if (location.href.includes("group_id=")) {
+    var reportActionForm = document.getElementsByTagName("form")[7];
+  } else if (
+    location.href.includes("mode=all") ||
+    location.href.includes("mode=attack") ||
+    location.href.includes("mode=defense")
+  ) {
+    var reportActionForm = document.getElementsByTagName("form")[7];
+  } else if (
+    location.href.includes("mode=trade") ||
+    location.href.includes("mode=support")
+  ) {
+    var reportActionForm = document.getElementsByTagName("form")[4];
+  } else if (
+    location.href.includes("mode=") ||
+    location.href.includes("mode=forwarded")
+  ) {
+    var reportActionForm = document.getElementsByTagName("form")[2];
   } else {
-      var reportActionForm = document.getElementsByTagName('form')[7];
+    var reportActionForm = document.getElementsByTagName("form")[7];
   }
 
-  const rowHtml = reportActionForm.getElementsByTagName('table')[1].cloneNode(true);
+  const rowHtml = reportActionForm
+    .getElementsByTagName("table")[1]
+    .cloneNode(true);
 
   // "Select all" Checkbox + Label
-  const tableReports = reportActionForm.getElementsByTagName('table')[0];
-  const cbxSelectAllHtml = document.getElementById('select_all').cloneNode(true);
-  const cbxSelectAllLabel = reportTable.rows[reportTable.rows.length -1].getElementsByTagName('label')[0].cloneNode(true);
+  const tableReports = reportActionForm.getElementsByTagName("table")[0];
+  const cbxSelectAllHtml = document
+    .getElementById("select_all")
+    .cloneNode(true);
+  const cbxSelectAllLabel = reportTable.rows[reportTable.rows.length - 1]
+    .getElementsByTagName("label")[0]
+    .cloneNode(true);
 
   // Änderungen am DOM vornehmen
   tableReports.before(rowHtml);
@@ -135,12 +207,14 @@ if (location.href.includes("view=") === false) {
   tableReports.before(cbxSelectAllLabel);
 
   [
-    "Raubzug löschen",
-    "Handel löschen",
-    "Unterstützung löschen",
+    "Alle löschen",
+    "RZ löschen",
+    "MP löschen",
+    "UT löschen",
     "AM löschen",
-    "Sonstiges löschen",
-    "Weitergeleitet archivieren",
+    "Sonst. löschen",
+    "WG annehmen",
+    "WG archivieren",
   ].forEach((name) => {
     let button = $(
       `<button class="FazeSorter" style="padding:5px; margin:5px;">${name}</button>`
